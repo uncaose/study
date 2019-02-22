809ae9ce3fac77f9dfe77a10c3ca69bab3bbcd04
ch@ddhinc.net <uncaose@gmail.com>
2019-02-22 12:02:18 +0900

    add item md

---
+++ nodejs/tslint.js
@@ -0,0 +1,11 @@
+{
+  "defaultSeverity": "error",
+  "extends": ["tslint-config-standard"],
+  "rules": {
+    "prefer-const": true,
+    "trailing-comma": false,
+    "semicolon": false,
+    "space-before-function-paren": [true, "never"],
+    "quotemark": [true, "double"]
+  }
+}
